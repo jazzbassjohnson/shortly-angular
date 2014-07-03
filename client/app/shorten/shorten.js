ed54b38d93ed89b258b637921e82f881ea09b536
Hack Reactor Students <garydotjs@gmail.com>
2014-07-02 22:27:09 -0700

    Add addLink function

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,16 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $scope.error;
+  $scope.link = {};
+  $scope.addLink = function(){
+    // if matches send to server
+    // ^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$
+    Links.addLinks($scope.link).then(function(resp){
+      $scope.loading = false;
+    },
+    function(resp){
+      $scope.error = 'WRONG URL DUDE!';
+    });
+  };
 });
