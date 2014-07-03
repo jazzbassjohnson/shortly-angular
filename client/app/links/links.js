59698836dbb2066771fdf2cef625de74519cb30d
Hack Reactor Students <garydotjs@gmail.com>
2014-07-02 22:19:57 -0700

    Add getLinks function

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,17 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  $scope.data = {links: ''};
+
+  $scope.getLinks = function(){
+    Links.getLinks().then(function(data){
+      $scope.data.links = data.data;
+    });
+  };
+
+  $scope.init = function(){
+    $scope.getLinks();
+  };
+
+  $scope.init();
 });
