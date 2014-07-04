b3aad1ab973b69e46faa53eab2918854138aadfe
Hack Reactor Students <garydotjs@gmail.com>
2014-07-03 20:22:11 -0700

    add style and design

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,6 +3,7 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, Links) {
   $scope.data = {links: ''};
 
+
   $scope.getLinks = function(){
     Links.getLinks().then(function(data){
       $scope.data.links = data.data;
@@ -12,6 +13,17 @@ angular.module('shortly.links', [])
   $scope.init = function(){
     $scope.getLinks();
   };
-
   $scope.init();
+
 });
+// .controller('DemoCtrl', function($scope, $aside) {
+//   // Show a basic aside from a controller
+//   var myAside = $aside({title: 'My Title', content: 'My Content', show: true});
+
+//   // Pre-fetch an external template populated with a custom scope
+//   var myOtherAside = $aside({scope: $scope, template: 'app/shorten/shorten.html'});
+//   // Show when some event occurs (use $promise property to ensure the template has been loaded)
+//   myOtherAside.$promise.then(function() {
+//     myOtherAside.show();
+//   })
+// });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,17 +1,22 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $route, $location, Links) {
   // Your code here
-  $scope.error;
+  $scope.audio = document.getElementById('successSound');
   $scope.link = {};
   $scope.addLink = function(){
     // if matches send to server
     // ^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$
     Links.addLinks($scope.link).then(function(resp){
       $scope.loading = false;
+      $scope.audio.play();
     },
     function(resp){
-      $scope.error = 'WRONG URL DUDE!';
+      console.log('Invalid url');
     });
   };
+  console.log($scope.audio);
+  $scope.audio.addEventListener('ended', function(){
+    $route.reload();
+  });
 });
